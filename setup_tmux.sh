f9ef1e8e7d99f5631f735877ce61ba923a787e47
Kevin Reber <kreber@linkedin.com>
2025-06-07 00:10:07 -0700

    updated script

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -5,6 +5,7 @@ set -e
 
 # --- Global Variables ---
 CONFIG_FILE="$HOME/.tmux.conf"
+TPM_PATH="$HOME/.tmux/plugins/tpm"
 
 # --- Confirmation Prompt ---
 read -r -p "Install tmux and setup configs? (Y/N): " response
@@ -22,8 +23,12 @@ brew update
 brew install tmux
 
 # --- 2. Install TPM ---
-echo "Installing TPM (tmux Plugin Manager)..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ -d "$TPM_PATH" ]; then
+  echo "TPM already installed at $TPM_PATH. Skipping installation."
+else
+  echo "Installing TPM (tmux Plugin Manager)..."
+  git clone https://github.com/tmux-plugins/tpm "$TPM_PATH"
+fi
 
 # --- 3. Copy the Configuration File ---
 echo "Copying the Configuration File..."
@@ -31,7 +36,9 @@ cp "tmux.conf" "$CONFIG_FILE"
 
 # --- 4. Install Plugins ---
 echo "Installing tmux plugins..."
-tmux start \; command-prompt "install" \; display "Installing plugins..."
+tmux new-session -d -s temp_session
+tmux send-keys -t temp_session:0 "$HOME/.tmux/plugins/tpm/bin/install_plugins" C-m
+tmux kill-session -t temp_session
 
 # --- 5. Source the Configuration ---
 echo "Sourcing the Configuration..."
